add0694c95801d5b65155b314ed6ef93474a479a
osbm <osmanfbayram@gmail.com>
2024-11-25 00:43:18 +0300

    update

--- flake.nix
+++ flake.nix
@@ -3,13 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    hackmatrix = {
+      url = "github:collinalexbell/HackMatrix";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self , nixpkgs ,... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+        inherit system;
+    };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    environment_packages = with pkgs; [
+      python312
+      python312Packages.google-api-python-client
+      python312Packages.google-auth-httplib2
+      python312Packages.google-auth-oauthlib
+    ];
+  in {
+    packages."${system}".hackmatrix = pkgs.stdenv {
+      name = "hackmatrix";
+      
+    };
 
   };
 }
